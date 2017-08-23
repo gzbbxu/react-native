b9478d30b351d46b5d64747801c6094cfd438112
XTZ-01708110006\Administrator <930325972@qq.com>
2017-08-23 20:14:47 +0800

    promise 机制

--- HelloWorldTest/LoginLeaf.android.js
+++ HelloWorldTest/LoginLeaf.android.js
@@ -42,10 +42,29 @@ export default class LoginLeaf extends React.Component{
     }]);
   }
   userPressAddressBook(){
-    DeviceEventEmitter.addListener('AndroidToRNMessage',this.handleAndridMessage.bind(this));
+    // 回掉函数
+    /*DeviceEventEmitter.addListener('AndroidToRNMessage',this.handleAndridMessage.bind(this));
     var {NaiveModules} = require('react-native');
     let ExampleInterface = NativeModules.ExampleInterface;
-    NativeModules.ExampleInterface.HandleMessage('testMessage');
+    NativeModules.ExampleInterface.HandleMessage('testMessage');*/
+    var {NaiveModules} = require('react-native');
+    let ExampleInterface = NativeModules.ExampleInterface;
+    NativeModules.ExampleInterface.HandleMessage('testMessage').then(
+        (reult)=>{
+            console.log('ok');
+            this.setState(()=>{
+              return{
+                otherMsg:reult,
+              };
+            });
+        }
+    ).catch(
+      (error)=>{
+        console.log(error);//打印异常信息
+        console.log(error.message);//打印android 异常信息
+        console.log(error.code);//打印自己提供的异常
+      }
+    );
   }
   handleAndridMessage(aMessage){
       console.log('handleAndridMessage:'+aMessage);
--- HelloWorldTest/android/app/src/main/java/com/helloworldtest/ExampleInterface.java
+++ HelloWorldTest/android/app/src/main/java/com/helloworldtest/ExampleInterface.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -14,16 +16,19 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 /**
  * Created by Administrator on 2017/8/21 0021.
+ * 消息机制 回掉函数
  */
 
 public class ExampleInterface extends ReactContextBaseJavaModule {
     private  ReactApplicationContext context;
+    Promise interfacePromise;
     private final ActivityEventListener activityEventListener = new BaseActivityEventListener(){
         @Override
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
           if(resultCode==Activity.RESULT_OK){
               String strData = data.getStringExtra("data");
-              sendMessage(strData);
+            //  sendMessage(strData);
+              interfacePromise.resolve(strData);
 
           }
         }
@@ -33,6 +38,11 @@ public class ExampleInterface extends ReactContextBaseJavaModule {
         this.context = reactContext;
         context.addActivityEventListener(activityEventListener);
     }
+
+    /**
+     * 回掉函数形式
+     * @param msg
+     */
     private void sendMessage(String msg){
         //像react native 侧发送消息
         context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("AndroidToRNMessage",msg);
@@ -43,12 +53,17 @@ public class ExampleInterface extends ReactContextBaseJavaModule {
         return "ExampleInterface";
     }
     @ReactMethod
-    public void HandleMessage(String msg){
+    public void HandleMessage(String msg,Promise promise){
+        this.interfacePromise = promise;
         Log.i("zhouke","received message from RN:"+msg);
         Intent intent = new Intent();
         intent.setClass(context,TestActivity.class);
        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivityForResult(intent,0,null);
 
+    }
+    @ReactMethod
+    public void aMethodForRN(String aPara, Callback callback){
+        callback.invoke();
     }
 }
